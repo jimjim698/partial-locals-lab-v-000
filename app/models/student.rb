99381a641fcef2cc5568a86e6e1edc2a8cd3937d
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-14 00:10:40 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,6 +15,7 @@ class Student < ActiveRecord::Base
   has_many :classrooms, through: :classroom_students
   
   def self.search(name)
+    if name.present?
    self.all.select do |student|
      student.name.downcase.include?(name.downcase)
     end 
