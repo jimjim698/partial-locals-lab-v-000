7c8bbe412246756cfedce11feacc5fa9569184d8
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-14 00:10:01 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,17 +14,15 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
   
-  #def self.search(name)
-   #self.all.map do |student|
-    #  student.name.downcase.include?(name.downcase)
-    #end 
-    #else 
-    #self.all
-#end 
- # end 
+  def self.search(name)
+   self.all.select do |student|
+     student.name.downcase.include?(name.downcase)
+    end 
+    else 
+    self.all
+end 
+  end 
  
-   def self.search(query)
-    query.present? ? self.all.select { |student| student.name.downcase.include?(query.downcase) } : self.all
-  end
+  
   
 end
